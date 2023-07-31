cfd50951307da48db03d880d549f31a6bc1e46f5
lines14 <a.lipatov@a-i.kz>
2023-07-31 17:57:42 +0600

    implement bot 2

--- bot/buttons.py
+++ bot/buttons.py
@@ -1,6 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-b1 = KeyboardButton('Сгенерировать')
+b1 = KeyboardButton('Сгенерировать!')
+b2 = KeyboardButton('Отмена')
 
 main_menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 main_menu_keyboard.add(b1)
+
+cancellation_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+cancellation_keyboard.add(b2)
