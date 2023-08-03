3b52329f78ea3dcf0ab86283a47592cd336d36ab
lines14 <a.lipatov@a-i.kz>
2023-08-03 14:27:49 +0600

    init database

--- bookmaker_parser_bot_without_webhook.py
+++ bookmaker_parser_bot_without_webhook.py
@@ -1,8 +1,10 @@
+from main.utils.DB.database_utils import DatabaseUtils
 from aiogram.utils import executor
 from bot.bot_base import dp
 from bot import handlers
 
 async def on_startup(_):
+    DatabaseUtils.sql_start()
     print('Бот успешно запущен!')
 
 handlers.register_handler_client(dp)
--- main/utils/DB/database_utils.py
+++ main/utils/DB/database_utils.py
@@ -1,61 +1,14 @@
-# import { createConnection } from 'mysql2/promise';
-# import logger from '../log/logger.js';
+import sqlite3 as sq
+from pathlib import Path
+destination = Path(__file__).resolve().parent.parent.parent.parent
 
-# class DatabaseUtils {
-#     constructor(host, user, password, database) {
-#         logger.log(`[info] ▶ connect to ${database} database`);
-#         createConnection({
-#             host,
-#             user,
-#             password,
-#             database,
-#         }).then((conn) => {
-#             this.connection = conn;
-#         });
-#     }
-
-#     async closeConnection() {
-#         logger.log(`[info] ▶ close connection to database`);
-#         await this.connection.end();
-#     }
-
-#     async sqlQuery(query, values, logString) {
-#         logger.log(logString);
-#         const [rows] = await this.connection.query(query, [values]);
-#         return rows;
-#     }
-
-#     async sqlGet(tableName, target='*', conditions='', values=[]) {
-#         const log = `[info] ▶ select data from ${tableName} table`;
-#         const query =`SELECT ${target} FROM ${tableName} ${conditions};`;
-#         return await this.sqlQuery(query, values, log);
-#     }
-
-#     async sqlAdd(tableName, dataObject) {
-#         const log = `[info] ▶ insert data to ${tableName} table`;
-#         const columnNames = Object.keys(dataObject);
-#         const values = Object.values(dataObject);
-#         const query = `INSERT INTO ${tableName} (${columnNames}) VALUES (?)`;
-#         return await this.sqlQuery(query, values, log);
-#     }
-
-#     async sqlRefresh(tableName, dataObject) {
-#         const log = `[info] ▶ replace data in ${tableName} table`;
-#         const columnNames = Object.keys(dataObject);
-#         const values = Object.values(dataObject);
-#         const query = `REPLACE INTO ${tableName} (${columnNames}) VALUES (?)`;
-#         await this.sqlQuery(query, values, log);
-#     }
-
-#     async sqlDelete(tableName, conditions='', values=[]) {
-#         const log = `[info] ▶ delete data from ${tableName} table`;
-#         const query =`DELETE FROM ${tableName} ${conditions};`;
-#         await this.sqlQuery(query, values, log);
-#     }
-
-#     async sqlEdit(tableName, target='*', conditions='', values=[]) {
-#         const log = `[info] ▶ update data in ${tableName} table`;
-#         const query =`UPDATE ${tableName} SET ${target} ${conditions};`;
-#         await this.sqlQuery(query, values, log);
-#     }
-# }
+class DatabaseUtils:
+    @staticmethod
+    def sql_start():
+        global base, cur
+        base = sq.connect(f'{destination}/resources/database.db')
+        cur = base.cursor()
+        if base:
+            print('[ОК] - База данных подключена!')
+        base.execute("CREATE TABLE IF NOT EXISTS names_mapping(original_name TEXT, short_name TEXT)")
+        base.commit()
