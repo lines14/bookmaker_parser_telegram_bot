aae4e7cd4bb9ac59c72ebd8d369305fcefd71914
lines14 <a.lipatov@a-i.kz>
2023-11-28 15:48:52 +0600

    update

--- main/driver/browser_factory.py
+++ main/driver/browser_factory.py
@@ -14,7 +14,7 @@ class BrowserFactory(classutilities.ClassPropertiesMixin):
                 options.add_argument('--incognito')
                 if (ConfigManager.get_config_data().is_headless):
                     options.add_argument('--no-sandbox')
-                    options.add_argument('--headless=new')
+                    # options.add_argument('--headless=new')
                     options.add_argument('--headless')
                     options.add_argument('--disable-gpu')
                     options.add_argument('--disable-setuid-sandbox')
