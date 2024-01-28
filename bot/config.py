5d2482fb24de0b6519f2e4225ea383def35d2ce2
Anton Lipatov <tonybass1414@yahoo.com>
2024-01-28 18:23:07 +0600

    remove config from .gitignore due to creating .env

---
+++ bot/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TG_TOKEN = os.getenv('TG_TOKEN')
+
+# webhook settings
+WEBHOOK_HOST=os.getenv('WEBHOOK_HOST')
+WEBHOOK_PATH=f'/{TG_TOKEN}'
+WEBHOOK_URL=f'https://{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = os.getenv('WEBAPP_HOST')
+WEBAPP_PORT = os.getenv('WEBAPP_PORT')
